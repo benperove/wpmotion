efa74b7f2bd96104dd95bd7b4c1ec035ac3d126f
root <root@ip-10-65-158-221.ec2.internal>
2014-08-15 12:20:41 +0000

    updated list of uninstall options

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php
 
-//if uninstall not called from WordPress exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
+//if uninstall not called from wordpress then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
 delete_option( 'wpmotion_plugin_version' );
@@ -9,6 +9,7 @@ delete_option( 'wpmotion_state' );
 delete_option( 'wpmotion_selected_host' );
 delete_option( 'wpmotion_sourcehost' );
 delete_option( 'wpmotion_installs' );
-delete_option( 'wpmotion_environment' );
 delete_option( 'wpmotion_create_account_pressed' );
 delete_option( 'wpmotion_license_key' );
+delete_option( 'wpmotion_maintenance_mode' );
+delete_option( 'wpmotion_destination_hosts' );
